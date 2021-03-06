b0c6785affab4b55eb8140500e72f9e8f9f8db24
isaacalderonCETYS <isaac.calderon@cetys.edu.mx>
2021-03-06 09:01:05 -0800

    Video API parte 1

--- src/routes/routeIndex.js
+++ src/routes/routeIndex.js
@@ -5,13 +5,41 @@ const router = Router();
 //const express = require(express);
 //const router = express.Router();
 
-//Texto plano
-router.get('/', (req,res) => {
-    res.send('Ruta root funcional');
-});
+const juegos = require('./data.json');
+console.log(juegos);
 //Json
-router.get('/john', (req,res) => {
-    res.json({"name":"John Doe"});
+router.get('/', (req,res) => {
+    res.json(juegos);
 });
+
+router.get('/:id', (req,res) => {
+    const {id} = req.params;
+    juegos.forEach(juego => {
+        if (juego.id == id){
+            res.json(juego)
+        }
+    });
+    
+    res.json(juegos);
+});
+
+router.post('/', (req,res) => {
+    const {title, version, genre} = req.body
+
+    if (title && version && genre){
+        const id = juegos.length + 1;
+        const nuevojuego = {...req.body, id};
+        
+        juegos.push(nuevojuego);
+        
+        // console.log(nuevojuego)
+
+        res.status(200).json(juegos);
+    }else{
+        res.status(500).json({error:'no data'});
+        res.send("Err32L No data");
+    }
+});
+
 
 module.exports = router;
