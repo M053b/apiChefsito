aa964cf5488e92f1e92758aec108f4bbac2b3ceb
M053B <mose_mxal@hotmail.com>
2021-03-07 20:48:13 -0800

    Revert "commitDudoso"
    This reverts commit 3dafbf318ff1f1f9f57fbd29b5c00c5df141d6f0, reversing
    changes made to d450bd4b04b6afdd6683e0d79b16b332155e50ab.

--- src/routes/routeIndex.js
+++ src/routes/routeIndex.js
@@ -1,59 +1,26 @@
 const { Router } = require('express');
 const router = Router();
 
-const juegos = require('./data.json')
 
-//Variante
-//const express = require(express);
-//const router = express.Router();
+const comidas = require('./data.json');
 
-//Rutas
-router.get('/', (req,res) => {
-    res.json(juegos);
+
+router.get('/receta', (req,res) => {
+    res.json(comidas);
 });
 
-//get con parametro
-router.get('/:id', (req,res) => {
-    //const id = req.params.id;
-    const {id} = req.params;
-    juegos.forEach(juego =>{
-        if(juego.id == id){
-            res.json(juego);
+
+router.get('/receta/:clase', (req,res) => {
+    const {clase} = req.params;
+    comidas.forEach(comida => {
+        console.log(comida.clase);
+        if (comida.clase.toUpperCase() == clase.toUpperCase()){
+            res.json(comida)
         }
     });
-});
-
-router.post('/', (req,res) => {
-    //Guardado de cuerpo de request
-    const {title, version, genre} = req.body;
-    console.log(req.body);
     
-    //Validacion de existencia de datos
-    if(title && version && genre){
-        //Genera id para entrada mas reciente
-        const id = juegos.length + 1;
-        const newJuego = {...req.body, id};
-        //console.log(newJuego);
-        juegos.push(newJuego);
-        res.status(200).json(juegos);
-    } else {
-        res.status(500).json({error:'Mal formato !title && version && genre!'});
-    }    
+    res.status(500).json({error:'Clase no encontrada'});
 });
 
-/*
-//Texto plano
-router.get('/', (req,res) => {
-    res.send('Ruta root funcional');
-});
 
-//Json
-router.get('/john', (req,res) => {
-     const data = {
-        "name":"John Doe",
-        "id":"1234"
-    };
-    res.json({"name":"John Doe"});
-});
-*/
 module.exports = router;
